4266755dab3b8edac510193bac4e9fd994ca9c29
Mehrzadnkz <merzadnikzad@gmail.com>
2026-01-01 23:16:18 +0330

    Create Defualt File's

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css'
+import { RouterProvider } from "react-router/dom";
+import { router } from "./routes";
+
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+);
