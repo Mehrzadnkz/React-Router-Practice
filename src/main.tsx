c6650114954c46fc709ff5686daa4127fd2d7a34
Mehrzadnkz <merzadnikzad@gmail.com>
2026-01-02 12:26:53 +0330

    Add BrowserRouter And Fix Show Content In Web Browser

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // اضافه کردن BrowserRouter
 import "./index.css";
 import Main_Content from "./routers/main-content";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Main_Content />
+    <BrowserRouter> {/* قرار دادن BrowserRouter */}
+      <Main_Content />
+    </BrowserRouter>
   </React.StrictMode>
 );
--- src/routers/main-content.tsx
+++ src/routers/main-content.tsx
@@ -4,8 +4,8 @@ function Main_Content() {
     return (
         <>
             <div className="border-b-2 p-4 flex justify-around items-center text-lg">
-                <Link to="/" className="hover:scale-115 hover:font-semibold hover:text-blue-600 duration-500 transform cursor-pointer">Home</Link>
-                <Link to="/products" className="hover:scale-115 hover:font-semibold hover:text-blue-600 duration-500 transform cursor-pointer">Products</Link>
+                <Link to={{ pathname: "/" }}>Home</Link>
+                <Link to={{ pathname: "/products" }}>Products</Link>
             </div>
         </>
     )
